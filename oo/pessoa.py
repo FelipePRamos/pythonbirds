4191f7b14ee86cac4889a947391e3d0d4c10975c
felipe <fpramos04@gmail.com>
2022-08-09 20:18:42 -0300

    Criando atributos de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self,*filhos, nome=None, idade=31):
+    olhos=2
+    def __init__(self, *filhos, nome=None, idade=31):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá{id(self)}'
 
 if __name__ == '__main__':
     felipe = Pessoa(nome='Felipe')
     luciano =Pessoa(felipe, nome='Luciano')
-    print(Pessoa.cumprimentar(luciano))
+    print(Pessoa.cumprimentar)
     print(id(luciano))
-    print(luciano.cumprimentar())
+    print(luciano.cumprimentar)
     print(luciano.nome)
     print(luciano.idade)
 
@@ -20,7 +22,14 @@ if __name__ == '__main__':
 
     luciano.sobrenome = 'Ramos'
     del luciano.filhos
+    luciano.olhos = 1
+    del luciano.olhos
     print(luciano.__dict__)
     print(felipe.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(felipe.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(felipe.olhos))
 
 

96ba33e46f6fdf3b390cfb77d6de180963a21b57
felipe <fpramos04@gmail.com>
2022-08-18 21:36:44 -0300

    Aula Método protegido

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,7 +6,7 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá{id(self)}'
+        return f'Olá, meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -16,12 +16,20 @@ class Pessoa:
     def nome_e_atributos_de_classes(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe=super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+
+class Mutante(Pessoa):
+   olhos = 3
+
 if __name__ == '__main__':
-    felipe = Pessoa(nome='Felipe')
-    luciano =Pessoa(felipe, nome='Luciano')
-    print(Pessoa.cumprimentar)
+    felipe = Mutante(nome='Felipe')
+    luciano = Homem(felipe, nome='Luciano')
+    print(Homem.cumprimentar(luciano))
     print(id(luciano))
-    print(luciano.cumprimentar)
+
     print(luciano.nome)
     print(luciano.idade)
 
@@ -34,7 +42,6 @@ if __name__ == '__main__':
     del luciano.olhos
     print(luciano.__dict__)
     print(felipe.__dict__)
-    Pessoa.olhos = 3
     print(Pessoa.olhos)
     print(luciano.olhos)
     print(felipe.olhos)
@@ -42,3 +49,9 @@ if __name__ == '__main__':
 
     print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classes(),luciano.nome_e_atributos_de_classes())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(felipe.olhos)
